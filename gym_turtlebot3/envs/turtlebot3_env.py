56fe561260797976c871313226df98a4bc7e0953
victorkich <victorkich@yahoo.com.br>
2022-01-14 17:20:55 -0300

    Add functions to get position and lidar distances.

--- gym_turtlebot3/envs/turtlebot3_env.py
+++ gym_turtlebot3/envs/turtlebot3_env.py
@@ -33,8 +33,8 @@ class TurtleBot3Env(gym.Env):
         self.env_stage = env_stage
         self.test_real = test_real
 
-        self.pub_cmd_vel = rospy.Publisher('cmd_vel_001', Twist, queue_size=5)
-        self.sub_odom = rospy.Subscriber('odom_001', Odometry, self.getOdometry)
+        self.pub_cmd_vel = rospy.Publisher('cmd_vel_002', Twist, queue_size=5)
+        self.sub_odom = rospy.Subscriber('odom_002', Odometry, self.getOdometry)
         self.sub_image = rospy.Subscriber('/usb_cam/image_raw', Image, self.getImage, queue_size=1)
 
         self.reset_proxy = rospy.ServiceProxy('gazebo/reset_simulation', Empty)
@@ -227,7 +227,7 @@ class TurtleBot3Env(gym.Env):
         data = None
         while data is None:
             try:
-                data = rospy.wait_for_message('/scan_001', LaserScan, timeout=5)
+                data = rospy.wait_for_message('/scan_002', LaserScan, timeout=5)
             except Exception:
                 pass
 
@@ -270,7 +270,7 @@ class TurtleBot3Env(gym.Env):
             data = None
             while data is None:
                 try:
-                    data = rospy.wait_for_message('scan_001', LaserScan, timeout=5)
+                    data = rospy.wait_for_message('scan_002', LaserScan, timeout=5)
                 except:
                     pass
 
@@ -287,7 +287,7 @@ class TurtleBot3Env(gym.Env):
             data = None
             while data is None:
                 try:
-                    data = rospy.wait_for_message('scan_001', LaserScan, timeout=5)
+                    data = rospy.wait_for_message('scan_002', LaserScan, timeout=5)
                 except:
                     pass
             state = self.getState(data)
