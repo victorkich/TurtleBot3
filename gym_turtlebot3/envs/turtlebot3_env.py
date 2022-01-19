3d3c8afd4992362ca7f2ba737a5dfd7a4efceb2e
victorkich <victorkich@yahoo.com.br>
2022-01-19 17:24:25 -0300

    Add functions to get position and lidar distances.

--- gym_turtlebot3/envs/turtlebot3_env.py
+++ gym_turtlebot3/envs/turtlebot3_env.py
@@ -22,7 +22,7 @@ TURTLE = '002'
 class TurtleBot3Env(gym.Env):
     def __init__(self, observation_mode=0, env_stage=1, max_env_size=None, continuous=False, observation_size=24,
                  action_size=5, min_range=0.1, max_range=2.5, min_ang_vel=-1.5, max_ang_vel=1.5, min_linear_vel=-0.5,
-                 max_linear_vel=0.5, goalbox_distance=0.15, collision_distance=0.08, reward_goal=200.,
+                 max_linear_vel=0.5, goalbox_distance=0.35, collision_distance=0.08, reward_goal=200.,
                  reward_collision=-20, angle_out=250, goal_list=None, test_real=False):
 
         self.goal_x = 0
