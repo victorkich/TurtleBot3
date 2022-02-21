677ca5db40b93492df4314f0c8e58d553a3c86e8
victorkich <victorkich@yahoo.com.br>
2022-02-21 17:08:21 -0300

    Add functions to get position and lidar distances.

--- gym_turtlebot3/envs/turtlebot3_env.py
+++ gym_turtlebot3/envs/turtlebot3_env.py
@@ -16,7 +16,7 @@ from gym.utils import seeding
 from gym_turtlebot3.envs.mytf import euler_from_quaternion
 from gym_turtlebot3.envs import Respawn
 
-TURTLE = '006'
+TURTLE = '003'
 
 
 class TurtleBot3Env(gym.Env):
